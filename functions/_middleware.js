0f51b1327c23972edb758a077f507727c135cbc8
Lutz Mitländer <lutz@mitlaender.dev>
2026-03-24 17:37:42 +0100

    Try if redirect works

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,39 +1,12 @@
 export async function onRequest(context) {
-  const response = await context.next(); // serve Hugo assets
+  const url = new URL(context.request.url);
 
-  const allowedOrigins = [
-    "https://mitlaender.dev",
-    "https://www.mitlaender.dev",
-  ];
-
-  const origin = context.request.headers.get("Origin");
-  if (origin && allowedOrigins.includes(origin)) {
-    response.headers.set("Access-Control-Allow-Origin", origin);
+  // Redirect www → apex
+  if (url.hostname === "dev-personalweb.lutzmitlaender.workers.dev") {
+    url.hostname = "google.de";
+    return Response.redirect(url.toString(), 301);
   }
 
-  response.headers.set("Access-Control-Allow-Methods", "GET, OPTIONS");
-  response.headers.set("Access-Control-Allow-Headers", "*");
-  response.headers.set("Access-Control-Max-Age", "86400");
-
-  return response;
-}
-
-export async function onRequestOptions(context) {
-  const allowedOrigins = [
-    "https://mitlaender.dev",
-    "https://www.mitlaender.dev",
-  ];
-
-  const origin = context.request.headers.get("Origin");
-  const headers = new Headers({
-    "Access-Control-Allow-Methods": "GET, OPTIONS",
-    "Access-Control-Allow-Headers": "*",
-    "Access-Control-Max-Age": "86400",
-  });
-
-  if (origin && allowedOrigins.includes(origin)) {
-    headers.set("Access-Control-Allow-Origin", origin);
-  }
-
-  return new Response(null, { status: 204, headers });
+  // Serve static assets from Hugo ./public folder
+  return context.next();
 }
