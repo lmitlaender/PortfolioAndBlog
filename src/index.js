ff5b972f29bb94e37398f9fd618c8754c9eb2858
Lutz Mitländer <lutz@mitlaender.dev>
2026-03-24 17:47:39 +0100

    Change to correct hostnames

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ export default {
     const url = new URL(request.url);
 
     // Redirect www → apex domain
-    if (url.hostname === "dev-personalweb.lutzmitlaender.workers.dev") {
-      url.hostname = "google.de";
+    if (url.hostname === "www.mitlaender.dev") {
+      url.hostname = "mitlaender.dev";
       return Response.redirect(url.toString(), 301);
     }
 
