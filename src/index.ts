5e3e9453dd454ed4451d6582c53c99c3c6be4e6f
Lutz Mitländer <lutz@mitlaender.dev>
2026-03-24 17:18:49 +0100

    Test

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,14 @@
-import { getAssetFromKV } from '@cloudflare/kv-asset-handler';
-
 export default {
-  async fetch(request: Request): Promise<Response> {
+  async fetch(request: Request, env: { ASSETS: Fetcher }): Promise<Response> {
     const url = new URL(request.url);
 
-    // Redirect www → apex domain
+    // redirect www -> apex
     if (url.hostname === "www.mitlaender.dev") {
       url.hostname = "mitlaender.dev";
       return Response.redirect(url.toString(), 301);
     }
 
-    // Serve Hugo assets from ./public
-    return getAssetFromKV({ request });
+    // serve Hugo assets from Pages
+    return env.ASSETS.fetch(request);
   },
 };
